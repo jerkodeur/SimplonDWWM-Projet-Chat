2b1066e18391523d375225056a2fafc143a653ea
JerkoMintHome <jerome.potie@gmail.com>
2021-06-02 12:33:43 +0200

    Resolved an error with pseudos array

--- model/message.php
+++ model/message.php
@@ -3,12 +3,12 @@
 function getMessages()
 {
     $pseudo = ['Adèle', 'Victor', 'Raymond', 'Claudia', 'Stéphanie'];
-    $length = 1;
+    $length = 0;
     $messages = [];
-    while ($length <= 5) {
-        $pos = $length === 1 ? 'er' : 'ème';
+    while ($length < 5) {
+        $pos = $length + 1 === 1 ? 'er' : 'ème';
         $messages[$length]['date'] = date('d-m-Y h:m:s');
-        $messages[$length]['message'] = "Ceci est le $length$pos message enregistré sur le chat";
+        $messages[$length]['message'] = "Ceci est le " . $length + 1 . $pos . " message enregistré sur le chat";
         $messages[$length]['pseudo'] = $pseudo[$length];
         $length++;
     }
